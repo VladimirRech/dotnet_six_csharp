3cf812f5deaf4d5c2ad73507af6e07926d21c279
Vladimir Rech <rechvladimir@hotmail.com>
2023-01-10 20:32:06 -0300

    Configuring to run multiple tests.

--- Program.cs
+++ Program.cs
@@ -5,22 +5,37 @@ namespace dotnet_six_csharp
     {
         public static int Main(string[] args)
         {
-            var test = new MyTestClass() { Id = 1, Name = "Teste 1" };
-
-            /// percorrer todas as propriedades da classe MyTest
-
-            System.Reflection.MemberInfo mi = typeof(MyTestClass).GetProperty("Id");
-
-            if (mi == null)
+            if (args.Length < 2)
                 return -1;
 
-            var att = Attribute.GetCustomAttribute(mi, typeof(MyAttributeClass)) as MyAttributeClass;
+            switch (args[1].ToUpper())
+            {
+                case "ATR":
+                    var test = new MyTestClass() { Id = 1, Name = "Teste 1" };
 
-            if (att == null)
-                return -1;
+                    /// percorrer todas as propriedades da classe MyTest
 
-            Console.WriteLine($"Propriedade: {mi.Name}, Coluna: {att.ColumnName}");
+                    System.Reflection.MemberInfo mi = typeof(MyTestClass).GetProperty("Id");
 
+                    if (mi == null)
+                        return -1;
+
+                    var att = Attribute.GetCustomAttribute(mi, typeof(MyAttributeClass)) as MyAttributeClass;
+
+                    if (att == null)
+                        return -1;
+
+                    Console.WriteLine($"Propriedade: {mi.Name}, Coluna: {att.ColumnName}");
+                    break;
+
+                case "FORMAT":
+                    var Ft = new FormatTest(10, 0, 'N');
+                    Console.WriteLine($"Teste de formatacao (inteiro): {Ft.Format(1003)}");
+                    Ft = new FormatTest(10, 3, 'N');
+                    Console.WriteLine($"Teste de formatacao (decimal): {Ft.Format(12.123m)}");
+                    Console.WriteLine($"Teste de formatacao (decimal): {Ft.Format(1234.123m)}");
+                    break;
+            }
 
             return 0;
         }
